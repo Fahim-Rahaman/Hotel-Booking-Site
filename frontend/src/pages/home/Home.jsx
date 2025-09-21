ace88d7bb0905f22dd6c1e1cc64d1455bc9dc1fe
Md Fahim Rahaman <sifatfahim2@gmail.com>
2025-09-21 20:00:20 +0200

    branch deleted

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -4,7 +4,6 @@ import Header from '../../components/header/Header'
 const Home = () => {
   return (
     <div>
-      <h1>Welcome to the Home Page</h1>
       <Navbar />
       <Header />
     </div>
