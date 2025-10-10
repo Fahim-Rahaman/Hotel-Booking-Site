7999b9c5f85c91ce31858b5453ee6126a4e7dd14
Md Fahim Rahaman <sifatfahim2@gmail.com>
2025-10-10 13:20:26 +0200

    mail list complete

---
+++ frontend/src/components/featuredProperty/featuredProperty.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import './featuredProperty.css'
+
+const featuredProperty = () => {
+    return (
+        <div className='featuredProperty'>
+            <div className="featuredPropertyItem">
+                <img src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&w=600&q=80" alt="" className="featuredPropertyImg" />
+                <span className="fpName" >Hotel Skymoon</span>
+                <span className="fpCity" >Madrid</span>
+                <span className="fpprice" >Starting from $1200</span>
+                <div className="fpRating">
+                    <button>8.9</button>
+                    <span>Excellent</span>
+                </div>
+            </div>
+            <div className="featuredPropertyItem">
+                <img src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&w=600&q=80" alt="" className="featuredPropertyImg" />
+                <span className="fpName" >Hotel Skymoon</span>
+                <span className="fpCity" >Madrid</span>
+                <span className="fpprice" >Starting from $1200</span>
+                <div className="fpRating">
+                    <button>8.9</button>
+                    <span>Excellent</span>
+                </div>
+            </div>
+            <div className="featuredPropertyItem">
+                <img src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&w=600&q=80" alt="" className="featuredPropertyImg" />
+                <span className="fpName" >Hotel Skymoon</span>
+                <span className="fpCity" >Madrid</span>
+                <span className="fpprice" > Starting from $1200</span>
+                <div className="fpRating">
+                    <button>8.9</button>
+                    <span>Excellent</span>
+                </div>
+            </div>
+            <div className="featuredPropertyItem">
+                <img src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&w=600&q=80" alt="" className="featuredPropertyImg" />
+                <span className="fpName" >Hotel Skymoon</span>
+                <span className="fpCity" >Madrid</span>
+                <span className="fpprice" >Starting from $1200</span>
+                <div className="fpRating">
+                    <button>8.9</button>
+                    <span>Excellent</span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default featuredProperty
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -3,6 +3,8 @@ import Navbar from '../../components/navbar/Navbar'
 import Header from '../../components/header/Header'
 import Featured from '../../components/featured/featured'
 import PropertyList from '../../components/propertyList/propertyList'
+import FeaturedProperty from '../../components/featuredProperty/featuredProperty'
+import MailList from '../../components/mailList/mailList'
 import './Home.css'
 const Home = () => {
   return (
@@ -13,6 +15,10 @@ const Home = () => {
         <Featured />
         <h1 className="homeTitle">Browse by property type</h1>
         <PropertyList />
+        <h1 className="homeTitle">Chosse your home</h1>
+        <FeaturedProperty />
+        <MailList />
+
       </div>
     </div>
   )
